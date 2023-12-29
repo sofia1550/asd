6d67e613c75938ccabe1629a59266469870bc157
sofia1550 <luccciuksofia@gmail.com>
2023-12-29 12:19:42 -0300

    d

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config(); // Esta línea carga las variables de entorno del archivo .env
+require('dotenv').config(); 
 
 const express = require('express');
 const http = require('http');
@@ -7,7 +7,9 @@ const { engine } = require('express-handlebars');
 const path = require('path');
 const mongoose = require('mongoose');
 const Product = require('./dao/models/products');
-// Conexión a MongoDB utilizando la variable de entorno
+const Message = require('./dao/models/message'); 
+
+// Conexión a MongoDB con la variable de entorno
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Conectado a MongoDB Atlas'))
   .catch(err => console.error('Error conectando a MongoDB Atlas:', err));
@@ -17,10 +19,11 @@ const server = http.createServer(app);
 const io = new Server(server);
 const handlebars = engine({
   runtimeOptions: {
-      allowProtoPropertiesByDefault: true,
-      allowProtoMethodsByDefault: true,
+    allowProtoPropertiesByDefault: true,
+    allowProtoMethodsByDefault: true,
   },
 });
+
 // Configuración de Handlebars y rutas estáticas
 app.engine('handlebars', handlebars);
 app.set('view engine', 'handlebars');
@@ -35,46 +38,62 @@ const productRoutes = require('./routes/products')(io);
 // Rutas
 app.get('/', async (req, res) => {
   try {
-      const products = await Product.find({});
-      res.render('home', { products });
+    const products = await Product.find({});
+    res.render('home', { products });
   } catch (error) {
-      console.error('Error al obtener productos:', error);
-      res.status(500).send('Error al cargar la página');
+    console.error('Error al obtener productos:', error);
+    res.status(500).send('Error al cargar la página');
   }
+});
+app.get('/chat', (req, res) => {
+  res.render('chat');
 });
 app.use('/api/products', productRoutes);
 app.use('/api/carts', cartRoutes);
-
 
 const PORT = 8080;
 server.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
   console.log('Un cliente se ha conectado');
 
-  const products = readProductsFile();
-  socket.emit('updateProductList', products);
+  // Cargar mensajes anteriores
+  try {
+    const messages = await Message.find({}).sort({ timestamp: -1 }).limit(50);
+    socket.emit('previousMessages', messages);
+  } catch (error) {
+    console.error('Error al recuperar mensajes:', error);
+  }
 
-  socket.on('addProduct', (newProductData) => {
-    const products = readProductsFile();
-    const newProduct = {
-      id: productsController.generateNewId(products),
-      ...newProductData
-    };
-    products.push(newProduct);
-    writeProductsFile(products);
-    io.emit('updateProductList', products);
-  });
-  socket.on('editProduct', (productId, updatedData) => {
-    productsController.updateProduct(productId, updatedData);
+
+  // Obtener y enviar la lista actualizada de productos desde MongoDB
+  Product.find({}).then(products => {
+    socket.emit('updateProductList', products);
   });
 
-  socket.on('deleteProduct', (productId) => {
-    productsController.deleteProduct(productId);
+  socket.on('newMessage', async (msg) => {
+    // Validar que el mensaje no esté vacío
+    if (msg.message.trim().length === 0) return;
+
+    try {
+      const newMessage = new Message(msg);
+      await newMessage.save();
+      // Emitir el mensaje a todos los sockets
+      io.emit('message', newMessage);
+    } catch (error) {
+      console.error('Error al guardar el mensaje:', error);
+    }
   });
-  socket.on('disconnect', () => {
-    console.log('Cliente desconectado');
+
+  // Emitir mensajes anteriores al nuevo cliente
+  socket.on('connection', async () => {
+    try {
+      const messages = await Message.find({}).sort({ timestamp: -1 }).limit(50);
+      socket.emit('previousMessages', messages);
+    } catch (error) {
+      console.error('Error al recuperar mensajes:', error);
+    }
   });
 });
