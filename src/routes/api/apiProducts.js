53c118abf2fafb08f76ee4463903c5e087cca334
Tu Nombre <tuemail@example.com>
2024-03-14 08:18:39 -0300

    f

--- src/routes/api/apiProducts.js
+++ src/routes/api/apiProducts.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const productsController = require('../../dao/mongo/productsController');
-const { authorize } = require('../../middleware/authorization'); 
+const { authorize } = require('../../middleware/authorization');
 
 module.exports = function (io) {
     router.get('/', productsController(io).getAllProducts);
     router.get('/:pid', productsController(io).getProductById);
-    router.post('/', authorize(['admin']), productsController(io).createProduct);
-    router.put('/:pid', authorize(['admin']), productsController(io).updateProduct);
-    router.delete('/:pid', authorize(['admin']), productsController(io).deleteProduct);
+
+    // Permitir que los administradores y los usuarios premium creen productos.
+    router.post('/', authorize(['admin', 'premium']), productsController(io).createProduct);
+
+    // Estas rutas requieren lógica adicional dentro de productsController para verificar la propiedad del producto
+    // en caso de usuarios premium, y permitir estas acciones para administradores sin restricciones.
+    router.put('/:pid', authorize(['admin', 'premium']), productsController(io).updateProduct);
+    router.delete('/:pid', authorize(['admin', 'premium']), productsController(io).deleteProduct);
 
     return router;
 };
